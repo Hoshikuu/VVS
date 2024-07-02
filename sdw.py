33036dc41e56201e799af7d8f6901965517df2ed
Hoshiku <145494860+Hoshikuu@users.noreply.github.com>
2024-07-02 12:22:14 +0200

    Update sdw.py

--- sdw.py
+++ sdw.py
@@ -4,16 +4,17 @@ import time
 
 def GetPid():
     for proc in psutil.process_iter():
-        print(proc)
         if (proc.name() == "vServer"):
             return proc.pid
     return -1
 
 def CheckServerStatus():
     if (GetPid() == -1):
+        print("Encendiendo Servidor")
         os.system("./Velneo-vServer/vServer.sh -s")
 
 if (__name__ == "__main__"):
+    print("Script Activado")
     while True == True:
         CheckServerStatus()
         time.sleep(5)
