dd6dfe20c7acf79ed2f8023eee37714d8e1f55cd
Hoshiku <145494860+Hoshikuu@users.noreply.github.com>
2024-07-02 12:14:12 +0200

    Update sdw.py

--- sdw.py
+++ sdw.py
@@ -4,18 +4,19 @@ import time
 
 def GetPid():
     for proc in psutil.process_iter():
-        print(proc)
         if (proc.name() == "vServer"):
             return proc.pid
     return -1
 
-if os.geteuid() != 0:
-    print("SUPE")
-    os.system("sudo su")
+def CheckServerStatus():
+    if (GetPid() == -1):
+        os.system("./Velneo-vServer/vServer.sh -s")
+
 if (__name__ == "__main__"):
     while True == True:
-        if (GetPid() == -1):
-            print("FAFAFA")
-            os.system("./Velneo-vServer/vServer.sh -s")
+        if os.geteuid() != 0:
+            os.system("sudo su")
+            CheckServerStatus()
+        else:
+            CheckServerStatus()
         time.sleep(5)
-
