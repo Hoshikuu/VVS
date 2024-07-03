25e1daf391f2bf9e183e25d5855fc7666ca6beed
Hoshiku <145494860+Hoshikuu@users.noreply.github.com>
2024-07-03 11:21:02 +0200

    Update sdw.py

--- sdw.py
+++ sdw.py
@@ -2,23 +2,60 @@ import psutil
 import os
 import time
 import threading
-    
+
+def CountHalfHour():
+    global timesStoped
+    global counter
+    localCounter = 0
+    while counter == True:
+        localCounter += 1
+        time.sleep(1)
+        if (localCounter == 30):
+            timesStoped = 0
+            localCounter = 0
+            break
+        print(localCounter)
 
 def GetPid():
-    for proc in psutil.process_iter():
+    for proc in psutil.process_iter(): 
         if (proc.name() == "vServer"):
             return proc.pid
     print("Servidor no detectado/n Posiblemente no encendido")
     return -1
 
 def CheckServerStatus():
-    if (GetPid() == -1):
-        print("Encendiendo Servidor")
-        os.system("./Velneo-vServer/vServer.sh -s")
+    global timesStoped
+    global check
+    global counter
+    global tCount
+    while check == True:
+        if (GetPid() == -1):
+            if (timesStoped == 0):
+                tCount = threading.Thread(target=CountHalfHour)
+                counter = True
+                tCount.start()
+            if (timesStoped == 3):
+                print("El servidor se ha parado demasiadas veces, Deteniendo")
+                check = False
+                counter = False
+                timesStoped = 0
+                break
+            timesStoped = timesStoped + 1
+            print("Encendiendo Servidor")
+            os.system("sudo sh Velneo-vServer/vServer.sh -s")
+        time.sleep(5)
 
 if (__name__ == "__main__"):
     print("Script Activado")
-    while True == True:
-        CheckServerStatus()
-        time.sleep(5)
+    timesStoped = 0
+    counter = False
+    check = False
+    
+    tCheck = threading.Thread(target=CheckServerStatus)
+    tCount = threading.Thread(target=CountHalfHour)
 
+    check = True
+    tCheck.start()
+
+    tCheck.join()
+    tCount.join()
